6459aacbd096c7163c6c91ec04cf91adce48debc
mu <59917266+4eUeP@users.noreply.github.com>
2024-04-02 17:25:49 +0800

    TestUtils: code refactoring

--- app/src/main/scala/hstream/server/KafkaBroker.scala
+++ app/src/main/scala/hstream/server/KafkaBroker.scala
@@ -33,84 +33,96 @@ class KafkaBroker(
 
   // TODO: TMP_FOR_HSTREAM
   def startup() = {
-    if (!sys.env.getOrElse("CONFIGS_FILE", "").trim.nonEmpty) {
+    if (!sys.env.getOrElse("CONFIG_FILE", "").trim.nonEmpty) {
       // TODO
       throw new NotImplementedError("KafkaBroker.startup")
     } else {
-      val spec =
-        config.testingConfig.getOrElse("spec", throw new IllegalArgumentException("spec is required")).asInstanceOf[Int]
-      if (spec == 1) {
-        val command = config.testingConfig
-          .getOrElse("command", throw new IllegalArgumentException("command is required"))
-          .asInstanceOf[String]
-        val image = config.testingConfig
-          .getOrElse("image", throw new IllegalArgumentException("image is required"))
-          .asInstanceOf[String]
-        val rmArg =
-          if (
-            config.testingConfig
-              .getOrElse("container_remove", throw new IllegalArgumentException("container_remove is required"))
-              .asInstanceOf[Boolean]
-          ) "--rm"
-          else ""
-        val storeDir = config.testingConfig
-          .getOrElse("store_dir", throw new IllegalArgumentException("store_dir is required"))
-          .asInstanceOf[String]
-        val dockerCmd =
-          s"docker run $rmArg -d --network host --name $containerName -v $storeDir:/data/store $image $command"
-        info(s"=> Start hserver by: $dockerCmd")
-        dockerCmd.run()
+      if (config.testingConfig.isEmpty) {
+        info("No testingConfig found, skip starting broker")
       } else {
-        throw new NotImplementedError("startup: spec is invalid!")
+        val spec =
+          config.testingConfig
+            .getOrElse("spec", throw new IllegalArgumentException("spec is required"))
+            .asInstanceOf[Int]
+        if (spec == 1) {
+          val command = config.testingConfig
+            .getOrElse("command", throw new IllegalArgumentException("command is required"))
+            .asInstanceOf[String]
+          val image = config.testingConfig
+            .getOrElse("image", throw new IllegalArgumentException("image is required"))
+            .asInstanceOf[String]
+          val rmArg =
+            if (
+              config.testingConfig
+                .getOrElse("container_remove", throw new IllegalArgumentException("container_remove is required"))
+                .asInstanceOf[Boolean]
+            ) "--rm"
+            else ""
+          val storeDir = config.testingConfig
+            .getOrElse("store_dir", throw new IllegalArgumentException("store_dir is required"))
+            .asInstanceOf[String]
+          val dockerCmd =
+            s"docker run $rmArg -d --network host --name $containerName -v $storeDir:/data/store $image $command"
+          info(s"=> Start hserver by: $dockerCmd")
+          dockerCmd.run()
+        } else {
+          throw new NotImplementedError("startup: spec is invalid!")
+        }
       }
     }
   }
 
   // TODO: TMP_FOR_HSTREAM
   def shutdown() = {
-    if (!sys.env.getOrElse("CONFIGS_FILE", "").trim.nonEmpty) {
+    if (!sys.env.getOrElse("CONFIG_FILE", "").trim.nonEmpty) {
       // TODO
       throw new NotImplementedError("KafkaBroker.shutdown")
     } else {
-      val spec =
-        config.testingConfig.getOrElse("spec", throw new IllegalArgumentException("spec is required")).asInstanceOf[Int]
-      if (spec == 1) {
-        // Remove broker container
-        if (
-          config.testingConfig
-            .getOrElse("container_remove", throw new IllegalArgumentException("container_remove is required"))
-            .asInstanceOf[Boolean]
-        ) {
-          s"docker rm -f $containerName".!
-        }
-
-        // Delete all logs
-        val storeAdminPort = config.testingConfig
-          .getOrElse("store_admin_port", throw new IllegalArgumentException("store_admin_port is required"))
-          .asInstanceOf[Int]
-        val deleteLogProc =
-          s"docker run --rm --network host hstreamdb/hstream bash -c 'echo y | hadmin-store --port $storeAdminPort logs remove --path /hstream -r'"
-            .run()
-        val code = deleteLogProc.exitValue()
-        // TODO: remove a non-exist log should be OK
-        // if (code != 0) {
-        //  throw new RuntimeException(s"Failed to delete logs, exit code: $code")
-        // }
-
-        // Delete all metastore(zk) nodes
-        val metastorePort = config.testingConfig
-          .getOrElse("metastore_port", throw new IllegalArgumentException("metastore_port is required"))
-          .asInstanceOf[Int]
-        s"docker run --rm --network host zookeeper:3.7 zkCli.sh -server 127.0.0.1:$metastorePort deleteall /hstream".!
+      if (config.testingConfig.isEmpty) {
+        info("No testingConfig found, skip starting broker")
       } else {
-        throw new NotImplementedError("shutdown: spec is invalid!")
+        val spec =
+          config.testingConfig
+            .getOrElse("spec", throw new IllegalArgumentException("spec is required"))
+            .asInstanceOf[Int]
+        if (spec == 1) {
+          // Remove broker container
+          if (
+            config.testingConfig
+              .getOrElse("container_remove", throw new IllegalArgumentException("container_remove is required"))
+              .asInstanceOf[Boolean]
+          ) {
+            s"docker rm -f $containerName".!
+          }
+
+          // Delete all logs
+          val storeAdminPort = config.testingConfig
+            .getOrElse("store_admin_port", throw new IllegalArgumentException("store_admin_port is required"))
+            .asInstanceOf[Int]
+          val deleteLogProc =
+            s"docker run --rm --network host hstreamdb/hstream bash -c 'echo y | hadmin-store --port $storeAdminPort logs remove --path /hstream -r'"
+              .run()
+          val code = deleteLogProc.exitValue()
+          // TODO: remove a non-exist log should be OK
+          // if (code != 0) {
+          //  throw new RuntimeException(s"Failed to delete logs, exit code: $code")
+          // }
+
+          // Delete all metastore(zk) nodes
+          val metastorePort = config.testingConfig
+            .getOrElse("metastore_port", throw new IllegalArgumentException("metastore_port is required"))
+            .asInstanceOf[Int]
+          s"docker run --rm --network host zookeeper:3.7 zkCli.sh -server 127.0.0.1:$metastorePort deleteall /hstream".!
+        } else {
+          throw new NotImplementedError("shutdown: spec is invalid!")
+        }
       }
     }
   }
 
   // TODO: TMP_FOR_HSTREAM
   def awaitShutdown() = {
-    if (!sys.env.getOrElse("CONFIGS_FILE", "").trim.nonEmpty) {
+    if (!sys.env.getOrElse("CONFIG_FILE", "").trim.nonEmpty) {
       // TODO
       throw new NotImplementedError("KafkaBroker.awaitShutdown")
     } else {
