9c190c9f5ac8f9929f4cb24a3bd2c6841d64c8e3
YangKian <1207783292@qq.com>
2024-04-08 18:40:41 +0800

    fix

--- app/src/test/scala/utils/kafka/KafkaServerTestHarness.scala
+++ app/src/test/scala/utils/kafka/KafkaServerTestHarness.scala
@@ -138,25 +138,30 @@ abstract class KafkaServerTestHarness extends QuorumTestHarness {
 
   def cleanPersistenceData(): Unit = {
     val config = configs.head
+    if (
+      config.testingConfig
+        .getOrElse("container_remove", throw new IllegalArgumentException("container_remove is required"))
+        .asInstanceOf[Boolean]
+    ) {
+      // Delete all logs
+      val storeAdminPort = config.testingConfig
+        .getOrElse("store_admin_port", throw new IllegalArgumentException("store_admin_port is required"))
+        .asInstanceOf[Int]
+      val deleteLogProc =
+        s"docker run --rm --network host hstreamdb/hstream bash -c 'echo y | hadmin-store --port $storeAdminPort logs remove --path /hstream -r'"
+          .run()
+      val code = deleteLogProc.exitValue()
+      // TODO: remove a non-exist log should be OK
+      // if (code != 0) {
+      //  throw new RuntimeException(s"Failed to delete logs, exit code: $code")
+      // }
 
-    // Delete all logs
-    val storeAdminPort = config.testingConfig
-      .getOrElse("store_admin_port", throw new IllegalArgumentException("store_admin_port is required"))
-      .asInstanceOf[Int]
-    val deleteLogProc =
-      s"docker run --rm --network host hstreamdb/hstream bash -c 'echo y | hadmin-store --port $storeAdminPort logs remove --path /hstream -r'"
-        .run()
-    val code = deleteLogProc.exitValue()
-    // TODO: remove a non-exist log should be OK
-    // if (code != 0) {
-    //  throw new RuntimeException(s"Failed to delete logs, exit code: $code")
-    // }
-
-    // Delete all metastore(zk) nodes
-    val metastorePort = config.testingConfig
-      .getOrElse("metastore_port", throw new IllegalArgumentException("metastore_port is required"))
-      .asInstanceOf[Int]
-    s"docker run --rm --network host zookeeper:3.7 zkCli.sh -server 127.0.0.1:$metastorePort deleteall /hstream".!
+      // Delete all metastore(zk) nodes
+      val metastorePort = config.testingConfig
+        .getOrElse("metastore_port", throw new IllegalArgumentException("metastore_port is required"))
+        .asInstanceOf[Int]
+      s"docker run --rm --network host zookeeper:3.7 zkCli.sh -server 127.0.0.1:$metastorePort deleteall /hstream".!
+    }
   }
 
 //   def recreateBrokers(reconfigure: Boolean = false, startup: Boolean = false): Unit = {
