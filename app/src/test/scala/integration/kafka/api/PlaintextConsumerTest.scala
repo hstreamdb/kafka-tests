0dddd4a71bb7a28d6bd79a0ec246cdc83d4cf326
mu <59917266+4eUeP@users.noreply.github.com>
2024-04-18 16:14:17 +0800

    workaround for timeouts

--- app/src/test/scala/integration/kafka/api/PlaintextConsumerTest.scala
+++ app/src/test/scala/integration/kafka/api/PlaintextConsumerTest.scala
@@ -513,7 +513,7 @@ class PlaintextConsumerTest extends BaseConsumerTest {
       consumer,
       () => callback.successCount >= count || callback.lastError.isDefined,
       "Failed to observe commit callback before timeout",
-      waitTimeMs = 10000
+      waitTimeMs = 600000 // waitTimeMs = 10000, FIXME: PULL_TIMEOUTS
     )
 
     assertEquals(None, callback.lastError)
@@ -2000,7 +2000,8 @@ class PlaintextConsumerTest extends BaseConsumerTest {
       consumer,
       () => consumer.assignment() == expectedAssignment.asJava,
       s"Timed out while awaiting expected assignment $expectedAssignment. " +
-        s"The current assignment is ${consumer.assignment()}"
+        s"The current assignment is ${consumer.assignment()}",
+      waitTimeMs = 600000 // FIXME: PULL_TIMEOUTS
     )
   }
 
